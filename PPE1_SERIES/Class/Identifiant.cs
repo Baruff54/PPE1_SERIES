53f1a7030a0791b80acf5693baca552cceecdaf6
ksoudier <74368968+ViciousCarpet@users.noreply.github.com>
2022-05-09 16:00:56 +0200

    Début des classes

---
+++ PPE1_SERIES/Class/Identifiant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPE1_SERIES.Class
+{
+    class Identifiant
+    {
+        private int id;
+        private string login;
+        private string mdp;
+
+        public Identifiant(int id, string login, string mdp)
+        {
+            this.id = id;
+            this.login = login;
+            this.mdp = mdp;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Login { get => login; set => login = value; }
+        public string Mdp { get => mdp; set => mdp = value; }
+    }
+}
---
+++ PPE1_SERIES/Class/Saison.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPE1_SERIES.Class
+{
+    class Saison
+    {
+        private int id;
+        private Serie laSerie;
+
+        public Saison(int id, Serie laSerie)
+        {
+            this.id = id;
+            this.laSerie = laSerie;
+        }
+
+        public int Id { get => id; set => id = value; }
+        internal Serie LaSerie { get => laSerie; set => laSerie = value; }
+    }
+}
